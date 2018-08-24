200ce59056688923a37e2925902cf20862fe3acc
Ben Strutt <strutt@physics.ucla.edu>
2018-08-24 22:24:13 +0000

    make default correct alignment script

--- src/nuphase-cfg.c
+++ src/nuphase-cfg.c
@@ -265,7 +265,7 @@ void nuphase_acq_config_init ( nuphase_acq_cfg_t * c)
   c->run_file = "/nuphase/runfile" ; 
   c->status_save_file = "/nuphase/last.st.bin"; 
   c->output_directory = "/data/" ; 
-  c->alignment_command = "cd /home/nuphase/nuphase_python/;  python align_adcs.py -m" ;
+  c->alignment_command = "cd /home/nuphase/nuphase_python/;  python align_adcs_beacon.py" ;
 
   c->load_thresholds_from_status_file = 1; 
 
